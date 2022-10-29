bc496710af21e06759eb8c9c6b345472a97bcfc3
Arina Kohanova <arinakokhanova@gmail.com>
2022-10-29 19:18:49 +0300

    first commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,10 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//MARK: - IB Outlets
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var firstValueLabel: UILabel!
+    @IBOutlet var secondValueLabel: UILabel!
+    @IBOutlet var thirdValueLabel: UILabel!
+    
+    @IBOutlet var firstSlider: UISlider!
+    @IBOutlet var secondSlider: UISlider!
+    @IBOutlet var thirdSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 20
+        setupSliders()
+        setupLabels()
+        
+    }
+    
+//MARK: - IB Actions
+    
+    @IBAction func rgbSliderChanged(_ sender: UISlider) {
+        changeViewColor()
+        firstValueLabel.text = firstSlider.value.formatted()
+        secondValueLabel.text = secondSlider.value.formatted()
+        thirdValueLabel.text = thirdSlider.value.formatted()
+    }
+    
+//MARK: - Methods
+    func changeViewColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(firstSlider.value), green: CGFloat(secondSlider.value), blue: CGFloat(thirdSlider.value), alpha: 1)
+    }
+    
+    private func setupSliders() {
+        firstSlider.minimumValue = 0
+        secondSlider.minimumValue = 0
+        thirdSlider.minimumValue = 0
+        firstSlider.maximumValue = 1
+        secondSlider.maximumValue = 1
+        thirdSlider.maximumValue = 1
+    }
+    private func setupLabels() {
+        firstValueLabel.text = firstSlider.value.formatted()
+        secondValueLabel.text = secondSlider.value.formatted()
+        thirdValueLabel.text = thirdSlider.value.formatted()
     }
 
 
